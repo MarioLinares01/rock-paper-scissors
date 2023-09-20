619d2568a30a9d3cd0d248a7312209d798e60038
Mario Linares <mariolinares109@gmail.com>
2023-09-20 09:53:45 -0700

    Add function to play one round of 'Rock Paper Scissors'

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 // This file contains the necessary functions to play `Rock Paper Scissors`.
 
+// This function will return the computers choice as a string('Rock', 'Paper', or 'Scissors')
 function getComputerChoice() { 
-    /* This function will return the computers choice as a
-    string('Rock', 'Paper', or 'Scissors') */
     let choice = Math.floor(Math.random() * 3); // get a random Int from 0 - 2
     if (choice == 0) {
-        console.log('Rock');
-        return 'Rock';
+        return 'rock';
     } else if (choice == 1) {
-        console.log('Paper');
-        return 'Paper';
+        return 'paper';
     } else {
-        console.log('Scissors');
-        return 'Scissors';
+        return 'scissors';
     }
+}
+
+// This function will play a single round and will return a string that declares the winner
+function playRound(playerSelection, computerSelection) {
+    let result = '';
+    if (playerSelection == computerSelection) {
+        result = "It's a draw!";
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        result = 'You loose! Paper beats Rock.'
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        result = 'You win! Rock beats Scissors.'
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = 'You win! Paper beats Rock';
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        result = 'You loose. Scissors beat Paper.'
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        result = "You loose! Rock beats scissors.";
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        result = 'You win! Scissors beat Rock.';
+    }
+    return result;
 }
